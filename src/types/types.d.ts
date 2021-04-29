03fd31717ecff8065fd22f78fbe4df12b76f1ffc
Paul Copplestone <pcopplestone@gmail.com>
2021-04-29 21:16:36 +0800

    cleans unused imports

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,4 @@
 import { RequestGenericInterface } from 'fastify'
-import { FromSchema } from 'json-schema-to-ts'
-
 export interface AuthenticatedRequest extends RequestGenericInterface {
   Headers: {
     authorization: string
