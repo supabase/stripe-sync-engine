32ad5ad20e236864afd95a29fffd816b17b08400
Paul Copplestone <pcopplestone@gmail.com>
2021-04-29 22:27:07 +0800

    Adds price test

--- test/webhooks.test.ts
+++ test/webhooks.test.ts
@@ -3,13 +3,16 @@ import app from '../src/app'
 import { createHmac } from 'crypto'
 import customer_updated from './stripe/customer_updated.json'
 import product_updated from './stripe/product_updated.json'
+import price_updated from './stripe/price_updated.json'
 
 const unixtime = Math.floor(new Date().getTime() / 1000)
 const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET || ''
 
 describe('/webhooks', () => {
+  /**
+   * customer.updated
+   */
   test('customer.updated', async () => {
-    // Calculate the signature using the UNIX timestamp, postData and webhook secret
     const signature = createHmac('sha256', stripeWebhookSecret)
       .update(`${unixtime}.${JSON.stringify(customer_updated)}`, 'utf8')
       .digest('hex')
@@ -27,8 +30,10 @@ describe('/webhooks', () => {
     expect(response.statusCode).toBe(200)
     expect(json).toMatchObject({ received: true })
   })
+  /**
+   * product.updated
+   */
   test('product.updated', async () => {
-    // Calculate the signature using the UNIX timestamp, postData and webhook secret
     const signature = createHmac('sha256', stripeWebhookSecret)
       .update(`${unixtime}.${JSON.stringify(product_updated)}`, 'utf8')
       .digest('hex')
@@ -40,6 +45,27 @@ describe('/webhooks', () => {
         'stripe-signature': `t=${unixtime},v1=${signature},v0=ff`,
       },
       payload: product_updated,
+    })
+
+    const json = JSON.parse(response.body)
+    expect(response.statusCode).toBe(200)
+    expect(json).toMatchObject({ received: true })
+  })
+  /**
+   * price.updated
+   */
+  test('price.updated', async () => {
+    const signature = createHmac('sha256', stripeWebhookSecret)
+      .update(`${unixtime}.${JSON.stringify(price_updated)}`, 'utf8')
+      .digest('hex')
+
+    const response = await app().inject({
+      url: `/webhooks`,
+      method: 'POST',
+      headers: {
+        'stripe-signature': `t=${unixtime},v1=${signature},v0=ff`,
+      },
+      payload: price_updated,
     })
 
     const json = JSON.parse(response.body)
