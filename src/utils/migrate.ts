dedc51952b3a57e293fe31e01f83462606ad3645
phamhieu <phamhieu1998@gmail.com>
2022-01-17 15:15:52 +0700

    fix: runMigrations with try/catch

--- src/utils/migrate.ts
+++ src/utils/migrate.ts
@@ -35,6 +35,11 @@ async function connectAndMigrate(
 
 export async function runMigrations(): Promise<void> {
   console.log('Running migrations')
-  await connectAndMigrate(config.DATABASE_URL, './db/migrations')
-  console.log('Finished migrations')
+  try {
+    await connectAndMigrate(config.DATABASE_URL, './db/migrations')
+  } catch (error) {
+    console.error('Migration failed:', error)
+  } finally {
+    console.log('Finished migrations')
+  }
 }
