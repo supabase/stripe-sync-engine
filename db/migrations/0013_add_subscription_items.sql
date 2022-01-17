d8d48f9ed5b706a2d466eca57656917ea085e86a
phamhieu <phamhieu1998@gmail.com>
2022-01-17 16:02:11 +0700

    feat: add subscription items table

---
+++ db/migrations/0013_add_subscription_items.sql
@@ -0,0 +1,11 @@
+create table if not exists "stripe"."subscription_items" (
+  "id" text primary key,
+  "object" text,
+  "billing_thresholds" jsonb,
+  "created" integer,
+  "metadata" jsonb,
+  "quantity" integer,
+  "price" text REFERENCES "stripe"."prices",
+  "subscription" text,
+  "tax_rates" jsonb
+);
