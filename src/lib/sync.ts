6b3765023cb52c924cafc6b2522c5d2a2497738a
phamhieu <phamhieu1998@gmail.com>
2022-01-17 20:56:26 +0700

    feat: sync subscription test data

--- src/lib/sync.ts
+++ src/lib/sync.ts
@@ -1,5 +1,6 @@
 import { fetchProducts, upsertProduct } from './products'
 import { fetchPrices, upsertPrice } from './prices'
+import { fetchSubscriptions, upsertSubscription } from './subscriptions'
 
 export async function syncProducts(): Promise<{ synced: number; lastId: string }> {
   let hasMore = true
@@ -31,6 +32,21 @@ export async function syncPrices(): Promise<{ synced: number; lastId: string }> 
     lastId += data[data.length - 1].id
     hasMore = chunk.has_more
   }
+
+  return { synced, lastId }
+}
+
+// For Test data only
+export async function syncSubscriptions(): Promise<{ synced: number; lastId: string }> {
+  let hasMore = true
+  let synced = 0
+  let lastId = ''
+
+  const chunk = await fetchSubscriptions()
+  const data = chunk.data
+  await Promise.all(data.map((x) => upsertSubscription(x)))
+  synced += data.length
+  lastId += data[data.length - 1].id
 
   return { synced, lastId }
 }
--- src/routes/sync.ts
+++ src/routes/sync.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { syncProducts, syncPrices } from '../lib/sync'
+import { syncProducts, syncPrices, syncSubscriptions } from '../lib/sync'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function routes(server: FastifyInstance) {
@@ -11,11 +11,13 @@ export default async function routes(server: FastifyInstance) {
     handler: async (request, reply) => {
       const products = await syncProducts()
       const prices = await syncPrices()
+      const subscriptions = await syncSubscriptions()
       return reply.send({
         statusCode: 200,
         ts: Date.now(),
         products,
         prices,
+        subscriptions,
       })
     },
   })
