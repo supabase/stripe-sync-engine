26da8dc03ba7d609cca86e2bdf01d34b3348e02c
phamhieu <phamhieu1998@gmail.com>
2022-02-08 15:32:46 +0700

    feat: backfill sync customers and invoices

--- src/routes/sync.ts
+++ src/routes/sync.ts
@@ -1,5 +1,11 @@
 import { FastifyInstance } from 'fastify'
-import { syncProducts, syncPrices, syncSubscriptions } from '../lib/sync'
+import {
+  syncProducts,
+  syncPrices,
+  syncSubscriptions,
+  syncCustomers,
+  syncInvoices,
+} from '../lib/sync'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function routes(server: FastifyInstance) {
@@ -11,13 +17,17 @@ export default async function routes(server: FastifyInstance) {
     handler: async (request, reply) => {
       const products = await syncProducts()
       const prices = await syncPrices()
+      const customers = await syncCustomers()
       const subscriptions = await syncSubscriptions()
+      const invoices = await syncInvoices()
       return reply.send({
         statusCode: 200,
         ts: Date.now(),
         products,
         prices,
+        customers,
         subscriptions,
+        invoices,
       })
     },
   })
