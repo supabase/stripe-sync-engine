d15e7457729885eb76e68222a54cd647ec492137
phamhieu <phamhieu1998@gmail.com>
2022-02-09 16:34:26 +0700

    feat: new sync routes supports all/daily/weekly/monthly

---
+++ src/routes/sync/daily.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify'
+import { syncBackfill } from '../../lib/sync'
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default async function routes(fastify: FastifyInstance) {
+  fastify.post('/daily', {
+    handler: async (request, reply) => {
+      var currentTimeInSeconds = Math.floor(Date.now() / 1000)
+      var dayAgoTimeInSeconds = currentTimeInSeconds - 60 * 60 * 24
+
+      const result = await syncBackfill(dayAgoTimeInSeconds)
+      return reply.send({
+        statusCode: 200,
+        ts: Date.now(),
+        ...result,
+      })
+    },
+  })
+}
