37dd07c1745b2869d549631fbabf2b06abf37de8
phamhieu <phamhieu1998@gmail.com>
2022-02-09 17:27:25 +0700

    chore: fix linter errors

--- src/routes/sync/weekly.ts
+++ src/routes/sync/weekly.ts
@@ -5,8 +5,8 @@ import { syncBackfill } from '../../lib/sync'
 export default async function routes(fastify: FastifyInstance) {
   fastify.post('/weekly', {
     handler: async (request, reply) => {
-      var currentTimeInSeconds = Math.floor(Date.now() / 1000)
-      var weekAgoTimeInSeconds = currentTimeInSeconds - 60 * 60 * 24 * 7
+      const currentTimeInSeconds = Math.floor(Date.now() / 1000)
+      const weekAgoTimeInSeconds = currentTimeInSeconds - 60 * 60 * 24 * 7
 
       const result = await syncBackfill(weekAgoTimeInSeconds)
       return reply.send({
