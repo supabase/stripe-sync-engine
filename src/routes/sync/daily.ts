2076260cac5f44b3561775e9ce1cae5cd395be4a
phamhieu <phamhieu1998@gmail.com>
2022-02-10 10:36:46 +0700

    feat: update sync routes with query params

--- src/routes/sync/daily.ts
+++ src/routes/sync/daily.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance } from 'fastify'
-import { syncBackfill } from '../../lib/sync'
+import { syncBackfill, SyncBackfillParams } from '../../lib/sync'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function routes(fastify: FastifyInstance) {
   fastify.post('/daily', {
     handler: async (request, reply) => {
+      const { object } = request.query as { object?: string }
       const currentTimeInSeconds = Math.floor(Date.now() / 1000)
       const dayAgoTimeInSeconds = currentTimeInSeconds - 60 * 60 * 24
-
-      const result = await syncBackfill(dayAgoTimeInSeconds)
+      const params = {
+        gteCreated: dayAgoTimeInSeconds,
+        object: object ?? 'all',
+      } as SyncBackfillParams
+      const result = await syncBackfill(params)
       return reply.send({
         statusCode: 200,
         ts: Date.now(),
--- src/routes/sync/monthly.ts
+++ src/routes/sync/monthly.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance } from 'fastify'
-import { syncBackfill } from '../../lib/sync'
+import { syncBackfill, SyncBackfillParams } from '../../lib/sync'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function routes(fastify: FastifyInstance) {
   fastify.post('/monthly', {
     handler: async (request, reply) => {
+      const { object } = request.query as { object?: string }
       const currentTimeInSeconds = Math.floor(Date.now() / 1000)
       const monthAgoTimeInSeconds = currentTimeInSeconds - 60 * 60 * 24 * 30
-
-      const result = await syncBackfill(monthAgoTimeInSeconds)
+      const params = {
+        gteCreated: monthAgoTimeInSeconds,
+        object: object ?? 'all',
+      } as SyncBackfillParams
+      const result = await syncBackfill(params)
       return reply.send({
         statusCode: 200,
         ts: Date.now(),
