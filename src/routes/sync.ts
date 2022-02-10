6a158b9408f627e9e970795942ccaf457bea488d
phamhieu <phamhieu1998@gmail.com>
2022-02-10 14:30:34 +0700

    fix: null request.body

--- src/routes/sync.ts
+++ src/routes/sync.ts
@@ -6,10 +6,11 @@ import Stripe from 'stripe'
 export default async function routes(fastify: FastifyInstance) {
   fastify.post('/sync', {
     handler: async (request, reply) => {
-      const { created, object } = request.body as {
-        created?: Stripe.RangeQueryParam
-        object?: string
-      }
+      const { created, object } =
+        (request.body as {
+          created?: Stripe.RangeQueryParam
+          object?: string
+        }) ?? {}
       const params = { created, object } as SyncBackfillParams
       const result = await syncBackfill(params)
       return reply.send({
