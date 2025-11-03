b9c1026c4508ee28f4d68c8225677ee835d84c5d
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2025-11-03 14:32:27 +0800

    Update vitest.config.ts

--- packages/fastify-app/vitest.config.ts
+++ packages/fastify-app/vitest.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    resolve: {
+      alias: [{ find: /(.*)\.js$/, replacement: '$1.ts' }],
+    },
     environment: 'node',
     deps: {
       inline: [/.*/], // inline deps for Vite to transform
